8efec98978462ade396bdcb8ca4c3367a11cdede
heitor <presuntozipper121@gmail.com>
2025-03-10 14:13:27 -0300

    test

--- app/src/main/java/com/easy/GeoWarden/repository/api/GeocoderAPIService.kt
+++ app/src/main/java/com/easy/GeoWarden/repository/api/GeocoderAPIService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 
 interface GeocoderAPIService {
 
-    @GET("http://api.openweathermap.org/geo/1.0/direct?q=São Paulo&limit=1&appid={key}")
+    @GET("http://api.openweathermap.org/geo/1.0/direct?q=São Paulo&limit=1&appid=c9eb2fcd4d3cacd6f08e55dcb45e3c59")
     fun locationData(): Call<JsonObject>
 }
 
